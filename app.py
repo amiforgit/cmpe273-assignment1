722061c5284215939eb723d054a97aa82cf4ed0c
Ami Patel <ami.patel317@gmail.com>
2017-03-16 14:27:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask
+import requests
+import sys
+import yaml
+import json
+import argparse
+from github3 import login
+from github3 import GitHub
+import base64
 
 app = Flask(__name__)
+f=sys.argv[1]
+g=GitHub()
 
+urlcon=f.split('/')
+l=len(urlcon)
+print(urlcon[l-1])
+print(urlcon[l-2])
+gh = g.login('amiforgit','#$123amigithub#$123')
+amiforgit=g.user('amiforgit')
+print(base64.b64decode(g.repository(urlcon[l-2],urlcon[l-1]).contents("/dev-config.yml").content))
+
+@app.route("/v1/<name>")
+def hello1(name):
+    if name.endswith('.yml'):
+        retval=base64.b64decode(g.repository(urlcon[l-2],urlcon[l-1]).contents(name).content)
+        return retval
+    if name.endswith('.yaml'):
+        name=name[:-5]+".yml"
+        retval=base64.b64decode(g.repository(urlcon[l-2],urlcon[l-1]).contents(name).content)
+        return retval
+    if name.endswith('.json'):
+        name=name[:-5]+".yml"
+        print(name)
+        retval=base64.b64decode(g.repository(urlcon[l-2],urlcon[l-1]).contents(name).content)
+        retvalf=(json.dumps(yaml.load(retval), sort_keys=True, indent=2))
+        return retvalf
 @app.route("/")
-def hello():
-    return "Hello from Dockerized Flask App!!"
-
+def hello2():
+   return "Hello from Dockerized Flask App!!"
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
